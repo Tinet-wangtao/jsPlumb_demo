85fa64314570ef8741e89e081df4106cad27e94f
Tinet-wangtao <wangtao@ti-net.com.cn>
2017-05-04 09:17:48 +0800

    添加一个初始模板

---
+++ js/metadata.js
@@ -0,0 +1,77 @@
+/**
+ * 元数据
+ */
+var metadata = [
+{
+	"play": {
+		"name":"播放",
+		"index":"play",
+		"type":"play",
+		"properties": {
+			"id": {
+				"des":"id"
+			},
+			"name": {
+				"des":"姓名"
+			},
+			"title": {
+				"des":"职务"
+			},
+			"phone": {
+				"des":"电话"
+			},
+			"email": {
+				"des":"邮箱"
+			}
+		}
+	}
+},
+{
+	"choose": {
+		"name":"选择",
+		"index":"choose",
+		"type":"choose",
+		"properties": {
+			"id": {
+				"des":"id"
+			},
+			"name": {
+				"des":"姓名"
+			},
+			"title": {
+				"des":"职务"
+			},
+			"phone": {
+				"des":"电话"
+			},
+			"email": {
+				"des":"邮箱"
+			}
+		}
+	}
+},
+{
+	"message": {
+		"name":"留言",
+		"index":"message",
+		"type":"message",
+		"properties": {
+			"id": {
+				"des":"id"
+			},
+			"name": {
+				"des":"姓名"
+			},
+			"title": {
+				"des":"职务"
+			},
+			"phone": {
+				"des":"电话"
+			},
+			"email": {
+				"des":"邮箱"
+			}
+		}
+	}
+}
+];
